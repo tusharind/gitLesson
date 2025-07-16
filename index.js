8bdca4a50cc9601acdb2a096c366797e45d211ff
tusharind <tusharj3255@gmail.com>
2025-07-16 16:11:30 +0530

    I made a silly website

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ console.log("hi")
 //U ka matlab hota hai untracked to remove this add and commit 
 
 function closure (){
-    
+    console.log("hi")
 }
+
+function biggerFunction (closure) {
+    let here = closure
+    here()
+
+}
+
+biggerFunction()
